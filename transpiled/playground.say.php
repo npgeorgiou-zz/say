f5c1ad6b7be910938dad14bf1c87b1ac801df731
nikos <nikolaos.konstantinos.pap@gmail.com>
2021-04-18 18:31:56 +0200

    stuff

--- transpiled/playground.say.php
+++ transpiled/playground.say.php
@@ -1,3 +1,11 @@
 <?php
 
 require_once './lib/all.php';
+$is_leap = Function_::from(function ($year) use(&$is_leap) {
+    global $is_leap;
+    $callMe(Integer_::from(1));
+    $callMe(Integer_::from(1));
+    $callMe(Integer_::from(1));
+    $callMe(Integer_::from(1));
+    $callMe(Integer_::from(1));
+});
